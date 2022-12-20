34e3b1581233679c3a5cb16c60b6349eced508e5
unknown <johirhaquedipok2@gmail.com>
2022-12-20 18:01:47 +0600

    post title with reusable form

--- src/components/enter-post-title.jsx
+++ src/components/enter-post-title.jsx
@@ -1,22 +1,34 @@
 import React from "react";
-import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { z } from "zod";
 import API from "../api/api-config";
 import { globalData } from "./context/Provider";
+import useForm from "./hook/useForm";
+import Form from "./reusable-component/form/form";
+import { Input } from "./reusable-component/form/input-field";
 import ProjectDetails from "./reusable-component/project-details";
 
+const postTitleSchema = z.object({
+  postTitle: z
+    .string()
+    .min(5, "post title must be more than 4 characters!")
+    .max(500, "post title must be less than 500 characters."),
+});
+
 const EnterPostTitle = () => {
-  const { register, handleSubmit } = useForm();
+  const form = useForm({ schema: postTitleSchema });
   const { setUserData } = globalData();
   const navigate = useNavigate();
 
   // post request
 
-  const onSubmit = async (data) => {
+  const handleSubmit = async (data) => {
     const newdata = { ...data, term: "term" };
+    console.log(data);
     const response = await API.post("/posts", {
       newdata,
     });
+    console.log(response);
     await setUserData(response?.data);
     navigate("/relevent");
   };
@@ -29,33 +41,30 @@ const EnterPostTitle = () => {
         <div className="divider"></div>
 
         {/* form */}
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="card flex-shrink-0 w-full max-w-5xl shadow-2xl bg-base-100">
-            <div className="card-body flex-row">
-              <label className="label self-start">
-                <span className="label-text text-2xl font-bold">
-                  Enter Post Title
-                </span>
-              </label>
-              <div className="form-control gap-4 flex-1">
-                <input
-                  type="text"
-                  placeholder="post title here"
-                  className="input input-bordered "
-                  {...register("postTitle", { required: true, maxLength: 20 })}
-                />
-                <div className="form-control inline-block">
-                  <button
-                    type="submit"
-                    className="btn btn-primary border-gray-600"
-                  >
-                    submit
-                  </button>
-                </div>
+
+        <Form form={form} onSubmit={handleSubmit}>
+          <div className="form-control gap-4 flex-1">
+            <Input
+              label="Post Title here"
+              type="text"
+              placeholder="post title here"
+              className="input input-bordered "
+              {...form.register("postTitle")}
+            />
+            {/* button */}
+            <div className="flex">
+              <div className="w-1/6"></div>
+              <div className="form-control inline-block w-2/3">
+                <button
+                  type="submit"
+                  className="btn btn-primary border-gray-600"
+                >
+                  submit
+                </button>
               </div>
             </div>
           </div>
-        </form>
+        </Form>
       </div>
     </div>
   );
