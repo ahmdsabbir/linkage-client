d494e9bb1be63f4696c32d7472da838978a4eeee
unknown <johirhaquedipok2@gmail.com>
2022-12-20 16:36:45 +0600

    form field validation

--- src/components/hook/useForm.jsx
+++ src/components/hook/useForm.jsx
@@ -1,9 +1,11 @@
 import { useForm as useHookForm } from "react-hook-form";
-
+// function to resolve zod schema we provide
+import { zodResolver } from "@hookform/resolvers/zod";
 // We provide additional option that would be our zod schema.
 
 const useForm = ({ schema, ...formConfig }) => {
   return useHookForm({
+    resolver: zodResolver(schema),
     ...formConfig,
   });
 };
--- src/components/reusable-component/form/test-form.jsx
+++ src/components/reusable-component/form/test-form.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import { z } from "zod";
 import useForm from "../../hook/useForm";
 import Form from "./form";
 import { Input } from "./input-field";
 
+const signUpFormSchema = z.object({
+  firstName: z.string().min(1, "First Name must be atleast 1 characters long!"),
+  username: z
+    .string()
+    .min(4, "Username must be atleast 1 characters long!")
+    .max(10, "Consider using shorter username."),
+  email: z.string().email("Please enter a valid email address."),
+  password: z
+    .string()
+    .min(6, "Please choose a longer password")
+    .max(256, "Consider using a short password"),
+});
+
 const TestForm = () => {
-  const form = useForm({});
+  const form = useForm({ schema: signUpFormSchema });
+  const handleSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <>
-      <Form form={form} onSubmit={(values) => console.log(values)}>
+      <Form form={form} onSubmit={handleSubmit}>
         <Input
           label="Your first name"
           type="text"
