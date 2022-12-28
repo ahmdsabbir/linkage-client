625763bd2d1a6598154ffea8924018ece32ea869
unknown <johirhaquedipok2@gmail.com>
2022-12-28 09:59:34 +0600

    spinner component

--- src/components/Pages/home.jsx
+++ src/components/Pages/home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Spinner from "../spinner";
 import ChosenTitleUrl from "./dashboard/user-dashboard/chosen-title-url";
 
 const Home = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-5xl font-bold text-center">Home page</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-5xl font-bold text-center mb-5">Home page</h1>
+      <Spinner />
       <ChosenTitleUrl />
     </div>
   );
