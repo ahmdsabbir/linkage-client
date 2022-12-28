8ca0515bc63860b98fbd8b0af7355148cdfa292c
johir haque dipok <johirhaquedipok2@gmail.com>
2022-12-28 19:45:21 +0600

    login homepage

--- src/components/Pages/home.jsx
+++ src/components/Pages/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import Spinner from "../spinner";
 import ChosenTitleUrl from "./dashboard/user-dashboard/chosen-title-url";
 
@@ -7,6 +8,7 @@ const Home = () => {
     <>
       <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="text-5xl font-bold text-center mb-5">Home page</h1>
+        <NavLink to={"/login"}>Login</NavLink>
         <Spinner />
         <ChosenTitleUrl />
       </div>
