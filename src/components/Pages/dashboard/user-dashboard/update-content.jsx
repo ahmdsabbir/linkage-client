b3d607baa4ced5ea8ba45a11b55963f37959f5ac
johir haque dipok <johirhaquedipok2@gmail.com>
2023-01-23 14:38:01 +0600

    udpdate content error handle

--- src/components/Pages/dashboard/user-dashboard/update-content.jsx
+++ src/components/Pages/dashboard/user-dashboard/update-content.jsx
@@ -142,11 +142,16 @@ const UpdateContent = () => {
     } catch (error) {
       dispatch({ type: "loading", payload: false });
       if (error?.response?.data?.msg) {
-        toast.error(error?.response?.data?.msg);
+        if (error?.response?.data?.msg == "Token has expired") {
+          handleLogout();
+          toast.error(error?.response?.data?.msg);
+        } else {
+          toast.error(error?.response?.data?.msg);
+        }
       } else if (error?.message == "Network Error") {
-        toast.error(error.message);
+        toast(error.message);
       } else {
-        toast.error(error.message);
+        toast(error.message);
       }
     }
   };
