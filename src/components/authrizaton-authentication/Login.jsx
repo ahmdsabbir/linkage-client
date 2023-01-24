35347ae2335fe68c81e609573b2bf14ae5a07ae2
johir haque dipok <johirhaquedipok2@gmail.com>
2023-01-24 19:51:26 +0600

    new dist

--- src/components/authrizaton-authentication/Login.jsx
+++ src/components/authrizaton-authentication/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { z } from "zod";
 import API from "../../api/api-config";
 import { useAppState } from "../context/AppProvider";
@@ -22,11 +22,18 @@ const loginFormSchema = z.object({
 
 const Login = () => {
   // react router dom hooks
-  const location = useLocation();
   const navigate = useNavigate();
 
   // Auth provider Context
   const { auth, setAuth } = useAuthState();
+  /*   const [plus, setPlus] = useState(null);
+  const handleError = () => {
+    setPlus((prev) => prev + 1);
+  };
+  function Bomb() {
+    throw new Error("kaboom");
+  } */
+
   // App state provider Context
   const {
     state: { loading },
@@ -87,6 +94,10 @@ const Login = () => {
     return (
       <>
         <div className="grid place-items-center h-screen ">
+          {/*  <button className="btn" onClick={handleError}>
+            {plus}
+          </button>
+          <div>{plus == 2 ? <Bomb /> : "working fine"}</div> */}
           <div className=" max-w-3xl rounded shadow-sm w-full">
             <div className="card-body">
               <h2 className="text-5xl font-semibold text-center mb-5 text-accent-dark">
--- src/error-boundary.jsx
+++ src/error-boundary.jsx
@@ -3,24 +3,36 @@ import React, { Component } from "react";
 export default class ErrorBoundary extends Component {
   constructor(props) {
     super(props);
-    this.state = { error: false, errorInfo: null };
+    this.state = { error: null };
   }
 
   static getDerivedStateFromError(error) {
-    return { hasError: false };
+    return { error };
   }
 
-  componentDidCatch(error, errorInfo) {
+  /*  componentDidCatch(error) {
     this.setState({
       error: error,
-      errorInfo: errorInfo,
     });
-  }
+  } */
 
   render() {
-    if (this.state.errorInfo) {
-      return <div>ErrorBoundary</div>;
+    const { error } = this.state;
+    console.log(error);
+    if (error) {
+      return <this.props.FallbackComponent error={error} />;
     }
+
     return this.props.children;
   }
+}
+
+export function ErrorFallback({ error }) {
+  console.log(error);
+  return (
+    <div>
+      <p>Something went wrong</p>
+      <pre>{error.message ? error.message : "sorry for the wrong"}</pre>
+    </div>
+  );
 }
