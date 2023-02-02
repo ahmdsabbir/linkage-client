63815a7f3434c89402d28a4b9ac0ea17033a6f45
johir haque dipok <johirhaquedipok2@gmail.com>
2023-02-02 17:54:39 +0600

    login testing

--- src/features/authrizaton-authentication/Login.tsx
+++ src/features/authrizaton-authentication/Login.tsx
@@ -32,6 +32,7 @@ const Login = () => {
                     />
                   </svg>
                 </span>
+
                 <input
                   type="email"
                   className="block w-full rounded-lg border bg-white py-3 px-11 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"
