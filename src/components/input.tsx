00951a93ffa7b62cc4a5916d5b52118c460d32e6
johir haque dipok <johirhaquedipok2@gmail.com>
2023-02-04 19:09:33 +0600

    passsowrd input field in login page

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -10,7 +10,7 @@ const Input = ({ type, placeholder, svg }: InputPropsFaces) => {
       {svg && svg}
       <input
         type={type}
-        className="block w-full rounded border bg-white py-3 px-11 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+        className="block w-full rounded border bg-primary/5 py-3 px-11 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
         placeholder={placeholder}
       />
     </div>
--- src/features/authrizaton-authentication/Login.tsx
+++ src/features/authrizaton-authentication/Login.tsx
@@ -1,38 +1,12 @@
 import Input from "../../components/input";
 
-const Login = ({ emailIcon, passwordIcon }: never) => {
-  const isIcon = true;
-
+const Login = () => {
   return (
     <div>
       {/* login form */}
 
       {/* reusable component */}
 
-      <Input
-        type={"text"}
-        placeholder={"Email address"}
-        svg={
-          <span className="absolute">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="mx-3 h-6 w-6 text-gray-300 "
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-              />
-            </svg>
-          </span>
-        }
-      />
-
-      {/* 
       <div>
         <section className="bg-white">
           <div className="container mx-auto flex min-h-screen items-center justify-center px-6">
@@ -46,26 +20,51 @@ const Login = ({ emailIcon, passwordIcon }: never) => {
               <h1 className="mt-3 text-2xl font-semibold capitalize text-gray-800 dark:text-white sm:text-3xl">
                 sign In
               </h1>
-              <div className="relative mt-8 flex items-center">
-                {isIcon && emailIcon}
+              <Input
+                type={"text"}
+                placeholder={"Email address"}
+                svg={
+                  <span className="absolute">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="mx-3 h-6 w-6 text-gray-300 "
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                      />
+                    </svg>
+                  </span>
+                }
+              />
+              <Input
+                type={"password"}
+                placeholder={"password here"}
+                svg={
+                  <span className="absolute">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="mx-3 h-6 w-6 text-gray-300 dark:text-gray-500"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                      />
+                    </svg>
+                  </span>
+                }
+              />
 
-                <input
-                  type="email"
-                  className={`block w-full rounded bg-dodger-blue/5 py-3  ${
-                    isIcon ? "px-11" : "px-4"
-                  } text-[#1B174D] focus:border-dodger-blue-400 focus:outline-none focus:ring focus:ring-dodger-blue-300 focus:ring-opacity-40  `}
-                  placeholder="Email address"
-                />
-              </div>
-              <div className="relative mt-4 flex items-center">
-                {isIcon && passwordIcon}
-
-                <input
-                  type="password"
-                  className="block w-full rounded  bg-dodger-blue/5 px-10 py-3 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-dodger-blue-300 focus:ring-opacity-40 dark:border-gray-600  "
-                  placeholder="Password"
-                />
-              </div>
               <div className="mt-6">
                 <button className="w-full transform rounded bg-dodger-blue-500 px-6 py-3 text-sm font-medium capitalize tracking-wide text-white transition-colors duration-300 hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
                   Sign in
@@ -83,7 +82,7 @@ const Login = ({ emailIcon, passwordIcon }: never) => {
             </form>
           </div>
         </section>
-      </div> */}
+      </div>
     </div>
   );
 };
