c7a3106acf948c74b59968eff39e3148e7dea7d0
johir haque dipok <johirhaquedipok2@gmail.com>
2023-02-06 15:38:43 +0600

    navbar icon color

--- src/pages/home/header.tsx
+++ src/pages/home/header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
                 viewBox="0 0 24 24"
                 strokeWidth="1.5"
                 stroke="currentColor"
-                className="h-6 w-6"
+                className="h-6 w-6 text-primary"
               >
                 <path
                   strokeLinecap="round"
