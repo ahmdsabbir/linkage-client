11ee99d2044acdc6943a2767cae2ce3ff9b71d3b
johir haque dipok <johirhaquedipok2@gmail.com>
2023-02-06 14:28:31 +0600

    html body to div

--- src/components/suggestions-card.tsx
+++ src/components/suggestions-card.tsx
@@ -11,7 +11,7 @@ const SuggestionsCard = ({ sourceTitle, sourceUrl }: PropsSuggestionsCard) => {
           <h3 className="text-xl font-medium text-gray-700">Greek breakfast</h3>
           <p className=" text-gray-400"> $8.99</p>
         </header> */}
-        <body className="flex flex-col space-y-5">
+        <div className="flex flex-col space-y-5">
           {/* title */}
           <div>
             <p className="text-lg font-medium text-gray-700">Title</p>
@@ -22,7 +22,7 @@ const SuggestionsCard = ({ sourceTitle, sourceUrl }: PropsSuggestionsCard) => {
             <p className="text-lg font-medium text-gray-700">URL</p>
             <p>{sourceUrl}</p>
           </div>
-        </body>
+        </div>
       </div>
       {/*  <!-- Action base sized basic button --> */}
       <div className="flex p-6 pt-0">
