2683fec845147efed0e5d8cd5bfaf2c435061bb4
johir haque dipok <johirhaquedipok2@gmail.com>
2023-02-09 16:29:04 +0600

    chosen title url onclick seletect

--- src/components/chosenTitleUrl.tsx
+++ src/components/chosenTitleUrl.tsx
@@ -48,9 +48,7 @@ const ChosenTitleUrl = () => {
           <textarea
             placeholder="chosen title..."
             className="  block h-32 w-full rounded-lg border border-gray-200 bg-white px-4 py-2.5 text-gray-700 placeholder-gray-400/70 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 "
-            defaultValue={
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!"
-            }
+            {...register("url")}
           />
           <p className="mt-3 text-xs text-gray-400 dark:text-gray-600">
             You can edit the post title as your needs before you hit the next
@@ -66,9 +64,7 @@ const ChosenTitleUrl = () => {
           <textarea
             placeholder="chosen title..."
             className="  block h-32 w-full rounded-lg border border-gray-200 bg-white px-4 py-2.5 text-gray-700 placeholder-gray-400/70 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 "
-            defaultValue={
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!"
-            }
+            {...register("title")}
           />
         </div>
       </div>
