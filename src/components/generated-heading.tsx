d710fe141910378d21b6ca8c24b2e9c32953ba1a
johir haque dipok <johirhaquedipok2@gmail.com>
2023-02-09 17:09:46 +0600

    generated heading sectiion update

--- src/components/generated-heading.tsx
+++ src/components/generated-heading.tsx
@@ -1,12 +1,16 @@
+import { useAppState } from "../context/update-post-context";
 import HeadingOrParagraph from "./heading-or-paragraph";
 
 const GeneratedHeading = () => {
+  const {
+    state: { generatedHeading },
+  } = useAppState();
   return (
     <>
       <HeadingOrParagraph
         sectionName={`Generated Heading`}
         sectionHelperText={`AI Generated Relevant Heading`}
-        generatedHeading={`All components can be copied and pasted and easily implemented in your tailwind css projects. You can choose which language you want to copy the desired component and just hover and click on the component you need and paste it on your project.`}
+        generatedHeading={generatedHeading}
       />
     </>
   );
