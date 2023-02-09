5c979585e646e8e0617af752c781fcc085d11475
johir haque dipok <johirhaquedipok2@gmail.com>
2023-02-09 10:27:06 +0600

    logout navigator in dashboard root layout

--- src/pages/user-dashboard/dashboard-layout.tsx
+++ src/pages/user-dashboard/dashboard-layout.tsx
@@ -1,7 +1,11 @@
-import { Outlet } from "react-router-dom";
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthState } from "../../context/auth-context";
 
 const DashboardLayout = () => {
-  return <Outlet />;
+  const { auth, handleLogout } = useAuthState();
+
+  return auth.token ? <Outlet /> : <Navigate to="/login" replace={true} />;
 };
 
 export default DashboardLayout;
