73f4dc01318fd4cb30402c84f6a7ad6e7fa2eec0
johir haque dipok <johirhaquedipok2@gmail.com>
2023-02-09 10:48:54 +0600

    dashboard details page card

--- src/pages/user-dashboard/dashboard-details.tsx
+++ src/pages/user-dashboard/dashboard-details.tsx
@@ -1,7 +1,60 @@
+import { NavLink } from "react-router-dom";
+
 const DashboardDetails = () => {
   return (
-    <div>
-      <h1 className="text-5xl text-gray-900"> Dashboard Details</h1>
+    <div className="grid place-content-center">
+      <div className="max-w-2xl overflow-hidden rounded-lg bg-white shadow-md dark:bg-gray-800">
+        <img
+          className="h-64 w-full object-cover"
+          src="https://images.unsplash.com/photo-1550439062-609e1531270e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt="Article"
+        />
+        <div className="p-6">
+          <div>
+            <span className="text-xs font-medium uppercase text-blue-600 dark:text-blue-400">
+              Hello,
+            </span>
+            <a
+              href="http://localhost:3000/"
+              className="mt-2 block transform text-xl font-semibold text-gray-800 transition-colors duration-300 hover:text-gray-600 hover:underline dark:text-white"
+              tabIndex={0}
+            >
+              Welcome to the dashboard
+            </a>
+            <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+              Please click the button to see your all projects
+            </p>
+            <NavLink
+              to={"/dashboard/all-projects"}
+              className={"btn-primary btn mt-5"}
+            >
+              View my Projects
+            </NavLink>
+          </div>
+          <div className="mt-4">
+            <div className="flex items-center">
+              <div className="flex items-center">
+                <img
+                  className="h-10 rounded-full object-cover"
+                  src="https://images.unsplash.com/photo-1586287011575-a23134f797f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=48&q=60"
+                  alt="Avatar"
+                />
+                <a
+                  href="http://localhost:3000/"
+                  className="mx-2 font-semibold text-gray-700 dark:text-gray-200"
+                  tabIndex={0}
+                >
+                  Jone Doe
+                </a>
+              </div>
+              <span className="mx-1 text-xs text-gray-600 dark:text-gray-300">
+                {new Date().getDate()} - {new Date().getMonth() + 1} -
+                {new Date().getFullYear()}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/user-dashboard/dashboard-layout.tsx
+++ src/pages/user-dashboard/dashboard-layout.tsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuthState } from "../../context/auth-context";
 
 const DashboardLayout = () => {
-  const { auth, handleLogout } = useAuthState();
+  const { auth } = useAuthState();
 
   return auth.token ? <Outlet /> : <Navigate to="/login" replace={true} />;
 };
