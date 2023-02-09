2f1d693a62f76ff4e6605592dced68f79ffea6d4
johir haque dipok <johirhaquedipok2@gmail.com>
2023-02-09 15:43:44 +0600

    suggesitions coponent update with api data

--- src/components/suggestions.tsx
+++ src/components/suggestions.tsx
@@ -1,20 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { Key } from "react";
+import { useAppState } from "../context/update-post-context";
 import SuggestionsCard from "./suggestions-card";
 
 const Suggestions = () => {
+  const {
+    state: { aiSuggestions },
+    dispatch,
+  } = useAppState();
+  console.log(aiSuggestions);
   return (
     <div className="container mx-auto flex min-h-screen items-center justify-center px-6">
       <div>
         <h2 className="my-3 text-2xl font-semibold capitalize text-gray-800  sm:text-3xl">
           Suggestions
         </h2>
-        <SuggestionsCard
-          sourceTitle={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!"
-          }
-          sourceUrl={
-            "https://www.google.com/search?q=tailwind+component+library&rlz=1C1UEAD_enBD1036BD1036&ei=OFzfY7bNOobiseMP8raRiAo&start=10&sa=N&ved=2ahUKEwi2ydCn7_38AhUGcWwGHXJbBKEQ8tMDegQIAhAE&biw=1920&bih=937&dpr=1"
-          }
-        />
+        {aiSuggestions.map(
+          (suggestion: {
+            post_id: Key | null | undefined;
+            title: string;
+            url: string;
+          }) => (
+            <SuggestionsCard
+              key={suggestion.post_id}
+              sourceTitle={suggestion.title}
+              sourceUrl={suggestion.url}
+            />
+          )
+        )}
       </div>
     </div>
   );
