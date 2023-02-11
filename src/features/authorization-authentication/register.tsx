abb868756aecfb4dccdd06124ee94e586880326c
johir haque dipok <johirhaquedipok2@gmail.com>
2023-02-11 20:34:52 +0600

    register component api

--- src/features/authorization-authentication/register.tsx
+++ src/features/authorization-authentication/register.tsx
@@ -1,11 +1,86 @@
-import { Link } from "react-router-dom";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { z } from "zod";
 import Input from "../../components/input";
 
+const RegisterFormSchema = z
+  .object({
+    username: z
+      .string()
+      .min(5, "Username must be more than 6 characters")
+      .max(25, "Consider using a short username"),
+    email: z.string().email("Please enter a valid email address."),
+    password: z
+      .string()
+      .min(6, "Please choose a longer password")
+      .max(256, "Consider using a short password"),
+    confirm: z.string(),
+  })
+  .refine((data) => data.password === data.confirm, {
+    path: ["confirm"],
+    message: "password did not match",
+  });
+
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setError,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm({ resolver: zodResolver(RegisterFormSchema) });
+
+  const navigate = useNavigate();
+
+  // axios post
+  const registerUser = async (
+    data
+  ): Promise<{
+    msg(msg: any): unknown;
+    data: unknown;
+  }> => {
+    const postData = JSON.stringify({
+      username: data.username,
+      email: data.email,
+      password: data.password,
+    });
+
+    const response = await primaryClient.post("api/auth/register", postData);
+
+    return response.data;
+  };
+
+  // mutation
+  const mutation = useMutation({
+    mutationFn: registerUser,
+    onSuccess: async (registered) => {
+      toast.success(
+        registered?.msg ? registered?.msg : "Registration Successfull"
+      );
+      navigate("/login");
+      reset();
+    },
+    onError: async (error) => {
+      const errorMsg = await errorFunc(error);
+      toast.error(errorMsg);
+    },
+  });
+
+  const handleRegisterSubmit = async (data) => {
+    mutation.mutate(data);
+  };
+
   return (
     <section className="bg-white ">
       <div className="container mx-auto flex min-h-screen items-center justify-center px-6">
-        <form className="w-full max-w-md">
+        <form
+          className="w-full max-w-md"
+          onSubmit={handleSubmit(handleRegisterSubmit)}
+        >
           <div className="mx-auto flex justify-center">
             <img
               className="h-7 w-auto sm:h-8"
@@ -43,6 +118,8 @@ const Register = () => {
                 </svg>
               </span>
             }
+            inputProps={register("username")}
+            error={errors.username?.message as string}
           />
 
           <Input
@@ -66,6 +143,8 @@ const Register = () => {
                 </svg>
               </span>
             }
+            inputProps={register("email")}
+            error={errors.email?.message as string}
           />
 
           <Input
@@ -89,6 +168,8 @@ const Register = () => {
                 </svg>
               </span>
             }
+            inputProps={register("password")}
+            error={errors.password?.message as string}
           />
           <Input
             type={"password"}
@@ -111,10 +192,18 @@ const Register = () => {
                 </svg>
               </span>
             }
+            inputProps={register("confirm")}
+            error={errors.confirm?.message as string}
           />
 
           <div className="mt-6">
-            <button className="btn-primary btn w-full">Register</button>
+            <button
+              className="btn-primary btn w-full"
+              disabled={mutation.isLoading ? true : false}
+            >
+              {" "}
+              {mutation.isLoading ? "Registering" : "Register"}{" "}
+            </button>
             <div className="mt-6 text-center ">
               <Link
                 to={"/login"}
