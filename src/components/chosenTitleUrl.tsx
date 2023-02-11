cf22b1c182726987957493d142718552ac317559
johir haque dipok <johirhaquedipok2@gmail.com>
2023-02-11 11:16:13 +0600

    chosen title hintText color styling

--- src/components/chosenTitleUrl.tsx
+++ src/components/chosenTitleUrl.tsx
@@ -103,7 +103,7 @@ const ChosenTitleUrl = () => {
               className="  block h-32 w-full rounded-lg border border-gray-200 bg-white px-4 py-2.5 text-gray-700 placeholder-gray-400/70 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 "
               {...register("title")}
             />
-            <p className="mt-3 text-xs text-gray-400 dark:text-gray-600">
+            <p className="dark:text-accent-300 mt-3 text-xs text-accent">
               You can edit the post title as your needs before you hit the next
               button !
             </p>
