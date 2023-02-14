bbe5d301368d13a6e9e1024e1a32c3937dd7d054
johir haque dipok <johirhaquedipok2@gmail.com>
2023-02-14 16:21:23 +0600

    copy to clipboard in selected project details

--- src/components/selected-project-details.tsx
+++ src/components/selected-project-details.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import { useAppState } from "../context/update-post-context";
 
 const SelectedProjectDetails = () => {
@@ -11,6 +13,8 @@ const SelectedProjectDetails = () => {
     },
   } = useAppState();
 
+  const [isCopied, setIsCopied] = useState(false);
+
   return (
     <>
       {selectedProject ? (
@@ -20,7 +24,7 @@ const SelectedProjectDetails = () => {
               <div className="flex flex-col  gap-4 ">
                 {selectedProject?.name && (
                   <div className="flex flex-col  font-medium ">
-                    <h1 className="text-lg text-gray-500">Project name:</h1>
+                    <h1 className="text-gray-500 text-lg">Project name:</h1>
                     <p className="text-md">{selectedProject.name}</p>
                   </div>
                 )}
@@ -78,18 +82,97 @@ const SelectedProjectDetails = () => {
 
                 {generatedHeading && (
                   <div className="flex flex-col gap-1">
-                    <span className="font-medium text-gray-800 ">
-                      Generated Heading:
+                    <span className="font-medium text-gray-500">
+                      Generated Heading
                     </span>
-                    <span className="">{generatedHeading}</span>
+                    <span className="inline-flex items-center justify-between space-x-1">
+                      <span className="">{generatedHeading}</span>
+                      <CopyToClipboard
+                        text={generatedHeading}
+                        onCopy={() => setIsCopied(true)}
+                      >
+                        <button className="rounded  border  p-2 text-gray-200 hover:bg-primary/25">
+                          {isCopied ? (
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="h-6 w-6 text-primary"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
+                              />
+                            </svg>
+                          ) : (
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="h-6 w-6 text-primary "
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"
+                              />
+                            </svg>
+                          )}
+                        </button>
+                      </CopyToClipboard>
+                    </span>
                   </div>
                 )}
                 {generatedParagraph && (
                   <div className="flex flex-col gap-1">
-                    <span className="font-medium text-gray-800">
-                      Generated Paragraph:
+                    <span>
+                      <span className="font-medium text-gray-800">
+                        {generatedParagraph}
+                      </span>
+                      <CopyToClipboard
+                        text={generatedParagraph}
+                        onCopy={() => setIsCopied(true)}
+                      >
+                        <button className="rounded  border  p-2 text-gray-200 hover:bg-primary/25">
+                          {isCopied ? (
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="h-6 w-6 text-primary"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
+                              />
+                            </svg>
+                          ) : (
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="h-6 w-6 text-primary "
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"
+                              />
+                            </svg>
+                          )}
+                        </button>
+                      </CopyToClipboard>
                     </span>
-                    <span className="">{generatedParagraph}</span>
                   </div>
                 )}
               </div>
