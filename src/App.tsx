dd9705b2affc1732edbc858a79b4d02d6f696f7e
johir haque dipok <johirhaquedipok2@gmail.com>
2023-02-18 16:09:06 +0600

    suspense for dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,30 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import CreateProject from "./components/create-project";
-import EditProject from "./components/edit-project";
+
 import ErrorPage from "./components/error-page";
 import Spinner from "./components/spinner";
 import Login from "./features/authorization-authentication/Login";
 import Register from "./features/authorization-authentication/register";
 import { ErrorMessage } from "./lib/error-fallback-message";
-import History from "./pages/dummy/History";
-import Reporters from "./pages/dummy/reporters";
-import Settings from "./pages/dummy/settings";
-import Users from "./pages/dummy/users";
 import Home from "./pages/home";
-import AllProjects from "./pages/user-dashboard/all-projects";
-import Basic from "./pages/user-dashboard/basic-page";
-import DashboardDetails from "./pages/user-dashboard/dashboard-details";
 
 import RootLayout from "./root-layout";
 
+const DashboardDetails = lazy(
+  () => import("./pages/user-dashboard/dashboard-details")
+);
 const DashboardLayout = lazy(
   () => import("./pages/user-dashboard/dashboard-layout")
 );
+const CreateProject = lazy(() => import("./components/create-project"));
+const EditProject = lazy(() => import("./components/edit-project"));
+const AllProjects = lazy(() => import("./pages/user-dashboard/all-projects"));
+const History = lazy(() => import("./pages/dummy/History"));
+const Reporters = lazy(() => import("./pages/dummy/reporters"));
+const Settings = lazy(() => import("./pages/dummy/settings"));
+const Users = lazy(() => import("./pages/dummy/users"));
+const Basic = lazy(() => import("./pages/user-dashboard/basic-page"));
 
 // error fallback component
 function ErrorFallback({ error }) {
@@ -40,19 +43,81 @@ const router = createBrowserRouter(
       <Route index element={<Home />} />
 
       {/* user dashboard */}
-      <Suspense fallback={<Spinner />}>
-        <Route path="dashboard" element={<DashboardLayout />}>
-          <Route index element={<DashboardDetails />} />
-          <Route path="create-project" element={<CreateProject />} />
-          <Route path="edit-project" element={<EditProject />} />
-          <Route path="all-projects" element={<AllProjects />} />
-          <Route path="basic" element={<Basic />} />
-          <Route path="history" element={<History />} />
-          <Route path="reporters" element={<Reporters />} />
-          <Route path="users" element={<Users />} />
-          <Route path="settings" element={<Settings />} />
-        </Route>
-      </Suspense>
+
+      <Route
+        path="dashboard"
+        element={
+          <Suspense fallback={<Spinner />}>
+            <DashboardLayout />
+          </Suspense>
+        }
+      >
+        <Route index element={<DashboardDetails />} />
+        <Route
+          path="create-project"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <CreateProject />
+            </Suspense>
+          }
+        />
+        <Route
+          path="edit-project"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <EditProject />
+            </Suspense>
+          }
+        />
+        <Route
+          path="all-projects"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <AllProjects />
+            </Suspense>
+          }
+        />
+        <Route
+          path="basic"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <Basic />
+            </Suspense>
+          }
+        />
+        <Route
+          path="history"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <History />
+            </Suspense>
+          }
+        />
+        <Route
+          path="reporters"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <Reporters />
+            </Suspense>
+          }
+        />
+        <Route
+          path="users"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <Users />
+            </Suspense>
+          }
+        />
+        <Route
+          path="settings"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <Settings />
+            </Suspense>
+          }
+        />
+      </Route>
 
       {/* user authentication */}
       <Route path="login" element={<Login />} />
