f08c7c0cbe990ccf0b7b8904bca324ca525c1b28
johir haque dipok <johirhaquedipok2@gmail.com>
2023-02-21 18:02:05 +0600

    silolinking table head styling

--- src/components/silo-linking-table.tsx
+++ src/components/silo-linking-table.tsx
@@ -6,11 +6,11 @@ const SiloLinkingTable = () => {
           <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
             <div className="overflow-hidden border border-gray-200  md:rounded-lg">
               <table className="min-w-full divide-y divide-gray-200 ">
-                <thead className="bg-gray-50 font-medium text-gray-800">
+                <thead className="bg-gray-50 font-medium text-gray-700 text-lg">
                   <tr>
                     <th
                       scope="col"
-                      className="py-3.5 px-4 text-left font-normal  text-sm rtl:text-right"
+                      className="py-3.5 px-4 text-left  rtl:text-right"
                     >
                       <div className="flex items-center gap-x-3">
                         <span>Source Post</span>
@@ -18,7 +18,7 @@ const SiloLinkingTable = () => {
                     </th>
                     <th
                       scope="col"
-                      className="px-12 py-3.5 text-left font-normal  text-sm rtl:text-right "
+                      className="px-12 py-3.5 text-left    rtl:text-right "
                     >
                       <button className="flex items-center gap-x-2">
                         <span>Target Post</span>
@@ -51,7 +51,7 @@ const SiloLinkingTable = () => {
                     </th>
                     <th
                       scope="col"
-                      className="px-4 py-3.5 text-left font-normal text-sm rtl:text-right "
+                      className="px-4 py-3.5 text-left  rtl:text-right "
                     >
                       <button className="flex items-center gap-x-2">
                         <span>Status</span>
@@ -73,13 +73,13 @@ const SiloLinkingTable = () => {
                     </th>
                     <th
                       scope="col"
-                      className="px-4 py-3.5 text-left font-normal  text-sm rtl:text-right"
+                      className="px-4 py-3.5 text-left   rtl:text-right"
                     >
                       Anchor
                     </th>
                     <th
                       scope="col"
-                      className="px-4 py-3.5 text-left font-normal text-sm rtl:text-right "
+                      className="px-4 py-3.5 text-left rtl:text-right "
                     >
                       Action
                     </th>
@@ -154,14 +154,14 @@ const SiloLinkingTable = () => {
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
                             viewBox="0 0 24 24"
-                            strokeWidth="1.5"
+                            strokeWidth={1.5}
                             stroke="currentColor"
-                            className="h-5 w-5"
+                            className="h-6 w-6"
                           >
                             <path
                               strokeLinecap="round"
                               strokeLinejoin="round"
-                              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                              d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
                             />
                           </svg>
                         </button>
@@ -174,14 +174,14 @@ const SiloLinkingTable = () => {
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
                             viewBox="0 0 24 24"
-                            strokeWidth="1.5"
+                            strokeWidth={1.5}
                             stroke="currentColor"
-                            className="h-5 w-5"
+                            className="h-6 w-6"
                           >
                             <path
                               strokeLinecap="round"
                               strokeLinejoin="round"
-                              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                              d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
                             />
                           </svg>
                         </button>
