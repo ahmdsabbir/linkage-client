a7fe41102f42981115c44b6538aa6689e7e0f249
johir haque dipok <johirhaquedipok2@gmail.com>
2023-02-25 13:58:49 +0600

    silo update target post table form

--- src/components/silo-update-target-post-table-row.tsx
+++ src/components/silo-update-target-post-table-row.tsx
@@ -1,28 +1,22 @@
-const SiloUpdateTargetPostTableRow = () => {
+const SiloUpdateTargetPostTableRow = ({ inputPost, inputUrl, inputTarget }) => {
   return (
     <tr>
       <td className="whitespace-nowrap px-4 py-4 font-medium text-gray-700 text-sm">
         <div className="inline-flex items-center gap-x-3">
           <div className="flex items-center gap-x-2">
             <div>
-              <span className="font-normal text-gray-700 text-sm ">
-                https://example.com/source-1
-              </span>
+              <input {...(inputPost ?? {})} disabled />
             </div>
           </div>
         </div>
       </td>
       <td className="whitespace-nowrap px-12 py-4 font-medium text-gray-700 text-sm">
         <div className="inline-flex items-center gap-x-2 rounded-full  px-3 py-1 ">
-          <span className="font-normal text-gray-500 text-sm">
-            https://example.com/the-pillar
-          </span>
+          <input {...(inputUrl ?? {})} disabled />
         </div>
       </td>
       <td className="whitespace-nowrap px-12 py-4 font-medium text-gray-700 text-sm">
-        <div className="inline-flex items-center gap-x-2 rounded-full  px-3 py-1 ">
-          <span className="font-normal text-gray-500 text-sm">1,2,3</span>
-        </div>
+        <input {...(inputTarget ?? {})} />
       </td>
     </tr>
   );
--- src/components/silo-update-target-post-table.tsx
+++ src/components/silo-update-target-post-table.tsx
@@ -1,41 +1,74 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-misused-promises */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 import SiloUpdateTargetPostTableRow from "./silo-update-target-post-table-row";
 
 const SiloTargetPostTable = () => {
+  const { register, handleSubmit, setValue } = useForm({
+    defaultValues: {
+      sourcePost: "bill",
+      sourceUrl: "luo",
+      targetPost: "1,2,3",
+    },
+  });
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => setValue("targetPost", json.userId))
+      .catch((error) => console.log(error));
+  }, [setValue]);
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <section className="container mx-auto px-4">
       <div className="mt-6 flex flex-col">
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
             <div className="overflow-hidden border border-gray-200  md:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-200 ">
-                <thead className="bg-gray-50 font-medium text-gray-700 text-lg">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="py-3.5 px-4 text-left  rtl:text-right"
-                    >
-                      <div className="flex items-center gap-x-3">
-                        <span>Source Post Url</span>
-                      </div>
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-12 py-3.5 text-left    rtl:text-right "
-                    >
-                      <span>Source Post Title</span>
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-4 py-3.5 text-left  rtl:text-right "
-                    >
-                      <span>Target Post</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-200 bg-white ">
-                  <SiloUpdateTargetPostTableRow />
-                </tbody>
-              </table>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <table className="min-w-full divide-y divide-gray-200 ">
+                  <thead className="bg-gray-50 font-medium text-gray-700 text-lg">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="py-3.5 px-4 text-left  rtl:text-right"
+                      >
+                        <div className="flex items-center gap-x-3">
+                          <span>Source Post Url</span>
+                        </div>
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-12 py-3.5 text-left    rtl:text-right "
+                      >
+                        <span>Source Post Title</span>
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-left  rtl:text-right "
+                      >
+                        <span>Target Post</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200 bg-white ">
+                    <SiloUpdateTargetPostTableRow
+                      inputPost={register("sourcePost")}
+                      inputUrl={register("sourceUrl")}
+                      inputTarget={register("targetPost")}
+                    />
+                  </tbody>
+                </table>
+                <button className="btn-primary btn">Click</button>
+              </form>
             </div>
           </div>
         </div>
